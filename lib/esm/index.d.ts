497cb3914bdbcebbbb2b6b4ce1cb0d9d8f5e334d
Benoit Simard <contact@bsimard.com>
2021-07-02 09:35:32 +0200

    Review publish process

--- lib/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Vertex, Edge } from "./types";
-import { cypherToGraph } from "./cypher-to-graph";
-import { cypherProjectionToGraph } from "./cypher-projection-to-graph";
-export { cypherToGraph, cypherProjectionToGraph, Vertex, Edge };
